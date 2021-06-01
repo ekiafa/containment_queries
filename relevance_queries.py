d5ce9de4c2472b80846e574ebd0496f64f10ef42
ekiafa <kiafaeftychia@gmail.com>
2021-06-01 19:22:12 +0300

    input files insertion

---
+++ relevance_queries.py
@@ -0,0 +1,24 @@
+import sys
+
+
+
+
+def main():      
+        
+        out=sys.argv[1]
+        =sys.argv[2]
+        source2=sys.argv[3]
+        source3=sys.argv[4]
+
+
+        with open('out.txt' ,mode='r') as pointers:
+
+
+
+
+
+
+if __name__ == '__main__':
+   
+   main()
+   
